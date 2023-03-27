9e0cbaac778da7ef75252a6bb06ffcc2d4763c13
DELL <DELL@DESKTOP-KLJI07S>
2023-03-27 15:05:46 +0700

    Add project files.

---
+++ exerciseProsedural_170/exerciseProsedural_170.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double mtk, double bing)
+{
+	return (mtk + bing) / 2;
+}
+string status(double d)
+{
+	if (d >= 80)
+		return "lulus";
+	if (d <= 70)
+		return "gagal";
+}
+
+void main() {
+
+	cout << "in\==================" << endl;
+	cout << "masukkan nilai mtk" << endl;
+	cout << "masukkan nilai bing" << endl;
+	cout << endl;
+}
+
+
+
